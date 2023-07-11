1dfbaaa69ad41a2373f12ffafda7589f2d2fd382
putrapuices <puices14@gmail.com>
2023-07-11 15:58:09 +0800

    Materi 12 eventhandling

---
+++ 12_eventhandling/Pratikum/lat12/src/components/global/navbar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return (
+        <>
+            <div
+                className='navbar'
+                style={{ backgroundColor: "#ffff", color: "#000" }}
+            >
+                <h3>Masukan Pekerjaan Anda</h3>
+            </div>
+        </>
+    );
+}
